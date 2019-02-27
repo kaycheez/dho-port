fdcc6e076e946a1f9fd374f54058f4e49caebe94
iDatLe <LeDatDac@gmail.com>
2019-02-27 11:48:37 -0800

    Added global css styling to start app

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
+import {Carousel} from '../Carousel/Carousel';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+      <Carousel />
       </div>
     );
   }
---
+++ src/Components/Carousel/Carousel.js
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+import styles from './Carousel.scss';
+
+export function Carousel () {
+    return (
+        <div className='carousel'>Hello</div>
+    )
+}
