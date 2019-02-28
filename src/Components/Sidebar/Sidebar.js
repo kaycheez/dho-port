56fb8714d664ae8195ed9a153bd81486bcfbe0ec
Kevin Chau <kevin.m.chau@gmail.com>
2019-02-27 23:05:21 -0800

    in process adding expanding list

--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,17 +1,38 @@
-import React, { Fragment } from 'react';
+import React, { useState, useEffect } from 'react';
 import './Sidebar.scss';
 
 const Sidebar = props => {
+  const examplePhotoCtgy = ['Weddings', 'Corporate', 'Portrait'];
+  const exampleVideoCtgy = ['Weddings', 'Dance'];
+
+
+  const [isExpanded, setIsExpanded] = useState(false);
+
+  const expandOptions = (options) => {
+    return options.map((option, i) => {
+      return (
+        <div key={i}>{option}</div>
+      )
+    })
+  }
+
+  // onClick={setIsExpanded(!isExpanded)}
+
   return (
     <nav className='sidebar'>
-      <header>
-        <h1>VISUAL BY DAVID HO</h1>
+      <header className='header'>
+        <h1>VISUALS BY DAVID HO</h1>
       </header>
-      <section>
-        <h2>Photos</h2>
-        <h2>Videos</h2>
+      <section className='section'>
+        <button >Photos</button>
+        {
+          isExpanded
+          ? expandOptions(examplePhotoCtgy)
+          : null
+        }
+        <div>Videos</div>
       </section>
-      <footer>
+      <footer className='footer'>
         <p>Contact</p>
       </footer>
     </nav>
