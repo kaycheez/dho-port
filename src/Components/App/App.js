019cd60436b246c9f83920c46febb66817fb915b
Kevin Chau <kevin.m.chau@gmail.com>
2019-02-27 18:43:29 -0800

    added light styling

--- src/Components/App/App.js
+++ src/Components/App/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import './App.css';
+import './App.scss';
 import Sidebar from '../Sidebar/Sidebar';
 
 class App extends Component {
--- src/Components/Sidebar/Sidebar.js
+++ src/Components/Sidebar/Sidebar.js
@@ -1,21 +1,20 @@
 import React, { Fragment } from 'react';
+import './Sidebar.scss';
 
 const Sidebar = props => {
   return (
-    <Fragment>
-      <nav className='sidebar'>
-        <header>
-          <h1>VISUAL BY DAVID HO</h1>
-        </header>
-        <section>
-          <h2>Photos</h2>
-          <h2>Videos</h2>
-        </section>
-        <footer>
-          <p>Contact</p>
-        </footer>
-      </nav>
-    </Fragment>
+    <nav className='sidebar'>
+      <header>
+        <h1>VISUAL BY DAVID HO</h1>
+      </header>
+      <section>
+        <h2>Photos</h2>
+        <h2>Videos</h2>
+      </section>
+      <footer>
+        <p>Contact</p>
+      </footer>
+    </nav>
   )
 }
 
