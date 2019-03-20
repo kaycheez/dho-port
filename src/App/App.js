ce0eefdc1ea235e594aed15be36bea4d0a7dd544
Kevin Chau <kevin.m.chau@gmail.com>
2019-03-19 18:05:37 -0700

    formatted files from 4 spaces to 2 spaces

--- src/App/App.js
+++ src/App/App.js
@@ -1,24 +1,16 @@
-import React, { Component } from 'react';
+import React from 'react';
 import styles from './App.module.scss';
 import Sidebar from './Components/Sidebar/Sidebar';
 import { CarouselBackground } from './Components/Carousel/CarouselBackground';
-import MappedRoutes from './routes/mappedRoutes';
-import routes from './routes/mappedRoutes';
 import { Switch } from 'react-router-dom';
 
-class App extends Component {
-  render() {
-    return (
-      <div className={styles.app}>
-          <CarouselBackground>
-              <Switch>
-                <MappedRoutes routes={routes} />
-              </Switch>
-          </CarouselBackground>
-          <Sidebar />
-      </div>
-    );
-  }
+const App = () => {
+  return (
+    <div className={styles.app}>
+      <CarouselBackground />
+      <Sidebar />
+    </div>
+  );
 }
 
 export default App;
--- src/App/Components/Carousel/CarouselData.js
+++ src/App/Components/Carousel/CarouselData.js
@@ -8,35 +8,35 @@ import fashion from '../../assets/images/fashion.jpg';
 import products from '../../assets/images/product.jpg';
 
 const carouselImages = [
-    {
-        image: corporate,
-        name: 'Corporate',
-        path: '/corporate'
-    }, 
-    {
-        image: events,
-        name: 'Events',
-        path: '/events'
-    }, 
-    {
-        image: fashion,
-        name: 'Fashion',
-        path: '/fashion'
-    }, 
-    {
-        image: products,
-        name: 'Products',
-        path: '/products'
-    }
+  {
+    image: corporate,
+    name: 'Corporate',
+    path: '/corporate'
+  },
+  {
+    image: events,
+    name: 'Events',
+    path: '/events'
+  },
+  {
+    image: fashion,
+    name: 'Fashion',
+    path: '/fashion'
+  },
+  {
+    image: products,
+    name: 'Products',
+    path: '/products'
+  }
 ]
 
 
-export const carouselData = carouselImages.map(({image, name, path}, i) => {
-    return (
-        <div className={styles.carousel__image} key={i}>
-            <img className={styles.carousel__imageItem} alt={name} src={image}/>
-                <h1 className={styles.carousel__imageCaption}>{name}</h1>
-                <Link to={path} className={styles.link}/>
-        </div>
-    )
+export const carouselData = carouselImages.map(({ image, name, path }, i) => {
+  return (
+    <div className={styles.carousel__image} key={i}>
+      <img className={styles.carousel__imageItem} alt={name} src={image} />
+      <h1 className={styles.carousel__imageCaption}>{name}</h1>
+      <Link to={path} className={styles.link} />
+    </div>
+  )
 })
