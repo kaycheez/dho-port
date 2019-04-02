57f8d3928de9e28b81bc95e0ae1e897245941dfe
iDatLe <LeDatDac@gmail.com>
2019-04-01 17:51:52 -0700

    Cleaned up code

--- src/App/Components/Gallery/Subgallery.js
+++ src/App/Components/Gallery/Subgallery.js
@@ -1,54 +1,52 @@
 import React, { useState } from 'react';
 import styles from './Gallery.module.scss';
-import LazyLoad from 'react-lazyload';
-
-const imagesLoaded = (parentNode) => {
-  const imgElements = parentNode.querySelectorAll("img")
-  for (let i = 0; i < imgElements.length; i++) {
-    const img = imgElements[i];
-    if (!img.complete) {
-      return false;
-    }
-  }
-  return true;
-}
 
 const Subgallery = (props) => {
 
   const [loading, setLoading] = useState(null);
   let galleryElement;
 
+  // Once all pictures have been completed, return true
+  const imagesLoaded = (parentNode) => {
+    const imgElements = parentNode.querySelectorAll("img")
+    for (let i = 0; i < imgElements.length; ++i) {
+      const img = imgElements[i];
+      if (!img.complete) {
+        return false;
+      }
+    }
+    return true;
+  }
+
 
   const mapImages = () => {
     return props.images.map((image, i) => {
       return (
           <div className={styles.sideGalleryItems} key={i}>
-
-              <img alt='Category' className={!loading ? styles.loadedImages : styles.noLoadedImages} src={image} onLoad={() => handleStateChange(galleryElement)}/>
+              <img 
+                alt='Category' 
+                src={image} 
+                className={!loading ? styles.loadedImages : styles.noLoadedImages} 
+                onLoad={() => handleStateChange(galleryElement)} 
+              />
           </div>
 
       )
     })
   }
 
-  // const renderSpinner = () => {
-  //   if (!loading) {
-  //     return <span className={styles.noSpinner} />
-  //   } else {
-  //     return <span className={styles.spinner} />
-  //   }
-  // }
-
+  // Once all pictures have been completed, set state to false
   const handleStateChange = (galleryElement) => {
     setLoading(!imagesLoaded(galleryElement))
   }
 
   return (
     <div ref={element => galleryElement = element}>
-    {/* {renderSpinner()} */}
+
       <div>
         {mapImages()}
       </div>
+
     </div>
   )
 
